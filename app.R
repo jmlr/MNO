ff12ce90dc006001c62c9c94c704a2419428442a
jmlr <jmlrichardson@gmail.com>
2024-08-08 09:52:29 -0400

    re-initializing

---
+++ app.R
@@ -0,0 +1,48 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("MNO Water Sampling Sites"),
+
+    # Sidebar with a slider input for number of bins 
+    mainPanel(leafletOutput("mymap")
+              )
+    
+)
+
+# Define server logic required to draw interactive map
+server <- function(input, output, session) {
+  
+  sites <- read.csv("allSites.csv")   
+  
+  customMarker <- makeAwesomeIcon(
+    icon = "chat-message",
+    library = "glyphicon",
+    markerColor = "darkblue",
+    iconColor = "white",
+    text = sites$Site)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+        setView(lng = -81.44, 
+                lat = 48.20, 
+                zoom = 5) |> 
+      addProviderTiles("CartoDB.Positron")  |>
+      addAwesomeMarkers(data = sites, icon = customMarker, lng=sites$Long, lat=sites$Lat, label = sites$Name)
+    })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
